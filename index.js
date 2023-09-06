15ef25369619e6146b40d42efb93b21f7a4ff238
Alin <at.silcock@gmail.com>
2023-09-06 11:56:04 -0600

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// const names = (["Guadalupe", "Ollie", "Aki"], "suprise");
+
+function writeCards(names) {
+    let greetings = []
+    for (let i = 0; i < names.length; i++) {
+        greetings.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    debugger;
+    }
+    return greetings;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--
+    }
+}
