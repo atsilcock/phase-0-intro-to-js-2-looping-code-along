7cf5bcf183a2fad7b41ec9af5b8a3f1e798acd66
Alin <at.silcock@gmail.com>
2023-10-07 13:52:25 -0600

    intro to looping

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // const names = (["Guadalupe", "Ollie", "Aki"], "suprise");
 
-function writeCards(names) {
-    let greetings = []
-    for (let i = 0; i < names.length; i++) {
-        greetings.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-    debugger;
-    }
-    return greetings;
+// Task 1
+// create a function writeCards
+// i = counter
+// 0 = start location
+// ++ adding 1 to i, everytime it loops
+
+function writeCards(names, event){
+    let cards = [] 
+    for (let i = 0; i<names.length; i++){
+       cards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        }
+        return cards;
 }
 
-function countDown(number) {
-    while (number >= 0) {
-        console.log(number);
-        number--
+function countDown(startnum){
+    var countdownvar = startnum
+    while (countdownvar >= 0){
+    console.log(countdownvar)
+    countdownvar--
     }
 }
